43a3b29361557649692d13e5d7f4cfe8d8238105
Jack Wu <pengqi.j.wu@gmail.com>
2017-02-26 22:31:46 -0800

    Add files via upload

---
+++ driver.py
@@ -0,0 +1,35 @@
+import math
+import h5py
+import numpy as np
+import matplotlib.pyplot as plt
+import spectral_clustering
+
+colors = ['red', 'blue', 'green', 'black', 'pink']
+k_hat = 7       # k_hat as defined in the slides
+
+def run(data_file, k, fig_file):
+	# data_file: contains n*d data matrix
+	# k: number of clusters
+	# fig_file: for saving the clustering results
+
+	A = np.genfromtxt(data_file, delimiter=',')  # data matrix
+	n = A.shape[0]  # number of data points
+	plt.figure()
+	
+	# To compute the similarity matrix 'W'
+	# and clustering assignment 'labels' with
+	# the following code
+	W = spectral_clustering.create_local_sim(A, k_hat)
+	for ncut in [False, True]:
+		labels = spectral_clustering.run(W, k, ncut)
+		plt.subplot(1, 2, ncut+1, aspect='equal')
+		for i in range(k):
+			subset = np.where(labels == i)[0]
+			plt.plot(A[subset, 0], A[subset, 1], 'o', c=colors[i], markeredgewidth=0.0, markersize=3)
+		plt.title('ncut={0}'.format(ncut))
+	
+	plt.tight_layout()
+	plt.savefig(fig_file)
+
+run('data1.txt', 3, 'spectral1.eps')
+run('data2.txt', 5, 'spectral2.eps')
---
+++ spectral_clustering.py
@@ -0,0 +1,53 @@
+import numpy as np
+from sklearn.cluster import KMeans
+
+
+def create_local_sim(A, k_hat):
+    # Creating a dense similarity matrix with local scale
+    # (sigma's) as shown in the slides, page 6
+    # A: n*d data matrix
+    # k_hat: as defined in the slide
+    # Return value: n*n similarity matrix
+    n = A.shape[0]
+    assert k_hat < n
+    a = (A*A).sum(axis = 1)
+    a = np.tile(a, (n,1)).transpose()
+    
+    x = A.dot(A.transpose())
+    
+    distance = a - 2*x + a.transpose()
+    distance = distance.astype(np.float64)
+    
+    # TODO
+    distance_copy = np.zeros((n,n))
+    distance_copy[:,:] = np.sqrt(distance[:,:])
+    distance_copy.sort()
+    kneighbor = distance_copy[:, k_hat]
+    kneighbor = np.tile(kneighbor, (n,1)).transpose()
+    tkneighbor = kneighbor.transpose()
+    sigmas = kneighbor * tkneighbor
+    W = pow(np.e, -distance/(2*sigmas))
+    
+    return W
+
+def run(W, k, ncut):
+    # Perform spectral clustering
+    # W: n*n similarity matrix
+    # k: number of clusters
+    # ncut: perform ratio cut when ncut==False; normalized cut when ncut==True
+    # Return value: n-vector that contains cluster assignments
+    assert W.shape[0] == W.shape[1]
+    n = W.shape[0]
+    d = W.sum(axis = 1)
+    D = np.diag(d)
+    L = D - W
+    if ncut == True:
+        L = L / L.max()
+    X, s, V = np.linalg.svd(L, full_matrices=False)
+    evecs = X[:,(n-k):n]
+    e_normed = evecs / np.tile(evecs.max(axis=1), (k,1)).transpose()
+    kmeans = KMeans(n_clusters=k, random_state=0).fit(e_normed)
+    # TODO
+    labels = kmeans.labels_
+    #np.zeros(n, dtype=np.uint32)
+    return labels
